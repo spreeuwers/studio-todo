625062c9e186a97522a9c0288425eb022ed58f2f
spreeuwers <noreply>
2015-07-05 17:08:36 +0000

    Add component comp1

---
+++ ui/comp1.reel/comp1.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/comp1.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Comp1
+ * @extends Component
+ */
+exports.Comp1 = Component.specialize(/** @lends Comp1# */ {
+    constructor: {
+        value: function Comp1() {
+            this.super();
+        }
+    }
+});
