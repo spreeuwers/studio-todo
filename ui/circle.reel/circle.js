ca4f73354188f00bbad166d616dd5e5f0ac78ba8
spreeuwers <noreply>
2015-07-05 16:53:20 +0000

    Add component circle

---
+++ ui/circle.reel/circle.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/circle.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Circle
+ * @extends Component
+ */
+exports.Circle = Component.specialize(/** @lends Circle# */ {
+    constructor: {
+        value: function Circle() {
+            this.super();
+        }
+    }
+});
